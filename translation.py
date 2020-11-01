afb6cd3bb7f59f9432a620b0add312755936ccfb
Jai Shree ram <73603048+jaishree-ram@users.noreply.github.com>
2020-11-01 13:08:27 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,15 +1,14 @@
 class Translation(object):
 
-
 #This will be appeared when anyone use start command
 
       START = """Hello {0}
 
-I am a converter by Unknown
+I am a converter clone of [Convert Ns Bot](https://telegram.dog/convert_Ns_bot) by {1}
 
 I can convert file to video or video to file with custom thumbnail support.
-"""
 
+"""
 
 #This will be appeared when anyone use help command
 
@@ -23,44 +22,71 @@ I can convert file to video or video to file with custom thumbnail support.
 
 4. Reply to file /converttovideo for converting into video.
 
+**SUPPORT GROUP:** [NS Bot Supporters](https://telegram.dog/Ns_Bot_supporters)
+
+"""
 
 #Please don't change this about command 🙏
 
       ABOUT = """
+
 **📝 Language:** Python 3
 
 **🧰 Framework:** Pyrogram
 
-**👨‍💻 Developer:** Anonymous
+**👨‍💻 Developer:** [Anonymous](https://t.me/Ns_AnoNymouS)
 
-**💻 Source Code:**Next Time
+**📮 Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+
+**👥 Group:** [NS BOT SUPPOTERS](https://t.me/Ns_Bot_supporters)
+
+**💻 Source Code:**[Press Me](https://github.com/Ns-AnoNymouS/TG-CONVERT-BOT)
+
 """
 
 ##############################################################################################################################
-##############################################################################################################################
 
+##############################################################################################################################
 
 #DON'T CHANGE THE NUMBERS IN THE FLOWER BRACKETS AND THE ORDER OF PERCENTAGE, DONE, TOTAL, SPEED, ETA ONLY CHANGE THE THEME 
 
       PROGRESS = """
+
 Percentage : {0}%
+
 Done ✅: {1}
+
 Total 🌀: {2}
+
 Speed 🚀: {3}/s
+
 ETA 🕰: {4}
+
 """
+
        
+
       DOWNLOAD_PROGRESS = "▪️"
+
       UPLOAD_PROGRESS = "▫️"
 
 ##############################################################################################################################
+
 ##############################################################################################################################
 
       DOWNLOAD_START = "Trying to Download 📥"
+
       DOWNLOAD_COMPLETE = "✅ Media Downloaded successfully\nPreparing for upload"
+
       UPLOAD_START = "Trying to Upload 📤"
+
       UPLOAD_COMPLETE = "THANKS FOR USING ME"
+
       SAVED_CUSTOM_THUMB_NAIL = "✅ Saved Thumbnail Successfully. This will be deleted in 24hrs"
+
       BANNED_TEXT = "YOU ARE BANNED. SO YOUR ARE NOT ABLE TO USE ME 🐒"
+
       REPLY_TEXT = "👩‍✈️ Reply to the media which you need to convert"
+
       DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Deleted Successfully ✅"
+
